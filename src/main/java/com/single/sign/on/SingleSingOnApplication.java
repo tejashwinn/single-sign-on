fb0ecf0d14edcd17ef5d78a9110245da0f9e3ae8
tejashwin.u <tejashwin5104@gmail.com>
2023-08-17 23:45:55 +0530

    init

---
+++ src/main/java/com/single/sign/on/SingleSingOnApplication.java
@@ -0,0 +1,13 @@
+package com.single.sign.on;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingleSingOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingleSingOnApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/single/sign/on/SingleSingOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.single.sign.on;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingleSingOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
